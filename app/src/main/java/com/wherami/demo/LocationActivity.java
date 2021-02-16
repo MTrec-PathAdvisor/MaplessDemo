c7cb0839771b1fc28814e1d6b1f3c45779444f0e
peterTsui <wltsuiac@connect.ust.hk>
2021-02-16 11:33:25 +0800

    init commit

---
+++ app/src/main/java/com/wherami/demo/LocationActivity.java
@@ -0,0 +1,72 @@
+package com.wherami.demo;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.StreamCorruptedException;
+
+import wherami.lbs.sdk.core.MapEngine;
+import wherami.lbs.sdk.core.MapEngineFactory;
+import wherami.lbs.sdk.data.Location;
+
+public class LocationActivity extends AppCompatActivity implements
+        MapEngine.LocationUpdateCallback { //Implements this interface to receive location update from the MapEngine instance
+
+
+    private static MapEngine engine=null;
+    private TextView textOut;
+    private Handler handler;
+    private Location mLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        textOut = (TextView) findViewById(R.id.textView);
+        handler = new Handler();
+        if(engine == null) {
+            engine = MapEngineFactory.Create(getApplicationContext());
+            try {
+                engine.initialize();
+            } catch (StreamCorruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        engine.attachLocationUpdateCallback(this);
+        engine.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        engine.stop();
+    }
+
+    @Override
+    public void onLocationUpdated(final Location location) {
+        new Thread() {
+            @Override
+            public void run() {
+                mLocation = location;
+                handler.post(runnableUi);
+            }
+        }.start();
+
+    }
+
+    Runnable runnableUi=new  Runnable(){
+        @Override
+        public void run() {
+            //更新界面
+            textOut.setText("Current Location: (" + mLocation.x + ", " + mLocation.y + ")");
+        }
+
+    };
+}
---
+++ app/src/main/java/com/wherami/demo/MainActivity.java
@@ -0,0 +1,160 @@
+package com.wherami.demo;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.StreamCorruptedException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import wherami.lbs.sdk.Client;
+import mtrec.*;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mainButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainButton = (Button) findViewById(R.id.main_button);
+
+        Log.d("MainActivity", "onCreate");
+
+        String[] allPermissions = null;
+        try {
+            allPermissions = getPackageManager()
+                    .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS)
+                    .requestedPermissions;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (allPermissions != null) {
+            boolean allPermissionsAlreadyGranted = true;
+
+            if (Build.VERSION.SDK_INT >= 23) {
+                List<String> permissions2request = new ArrayList<>();
+                for (String permission : allPermissions) {
+                    if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                        allPermissionsAlreadyGranted = false;
+                        permissions2request.add(permission);
+                    }
+                }
+
+                if (!permissions2request.isEmpty()) {
+                    ActivityCompat.requestPermissions(this,
+                            permissions2request.toArray(new String[0]),
+                            0xFFF);
+                }
+            }
+
+            Log.d("MainActivity", "allPermissionsAlreadyGranted = " + allPermissionsAlreadyGranted);
+            if(allPermissionsAlreadyGranted){
+                initialize();
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        initialize();
+    }
+
+    public void onButtonClicked(View view){
+        Intent intent = new Intent(MainActivity.this, LocationActivity.class);
+        startActivity(intent);
+    }
+    private void initialize(){
+        Log.d("MainActivity", "initialize");
+        try {
+            // The Dataset is downloaded from a server
+            // This is a development server serves as debug purpose only
+            // One should setup a http(s) server and host the files under
+            // http(s)://<host>/generated_assets/SciencePark-1719W/offline_data/
+            Client.Configure("http://143.89.199.79", "HKUST_v2", this);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (StreamCorruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        checkDataUpdate();
+    }
+
+    private void checkDataUpdate(){
+        //Start only when the app has the latest data
+        mainButton.setEnabled(false);
+        mainButton.setText("Checking Update...");
+        Log.d("MainActivity", "checkDataUpdate");
+        Client.CheckDataUpdate(new Client.DataUpdateQueryCallback() {
+            @Override
+            public void onQueryFailed(Exception e) {
+                Log.d("MainActivity", "onQueryFailed");
+                Toast.makeText(MainActivity.this, "Failed to check data update", Toast.LENGTH_LONG);
+                if(Client.GetDataVersion() != null){
+                    //It is possible to continue by using old data
+                    mainButton.setEnabled(true);
+                    mainButton.setText("START");
+                }else{
+                    //If no data is downloaded previously, it is impossible to continue. Please retry under network environment
+                }
+            }
+
+            @Override
+            public void onUpdateAvailable(String s) {
+                mainButton.setText("Updating data...");
+                Log.d("MainActivity", "onUpdateAvailable");
+
+                Client.UpdateData(new Client.DataUpdateCallback() {
+                    @Override
+                    public void onProgressUpdated(int i) {
+                        Log.d("MainActivity", "onProgressUpdated");
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        Log.d("MainActivity", "onCompleted");
+                        Toast.makeText(MainActivity.this, "Update succeeded", Toast.LENGTH_SHORT);
+                        mainButton.setEnabled(true);
+                        mainButton.setText("START");
+                    }
+
+                    @Override
+                    public void onFailed(Exception e) {
+                        Log.d("MainActivity", "onFailed");
+                        Toast.makeText(MainActivity.this, "Failed to update data", Toast.LENGTH_LONG);
+                        if(Client.GetDataVersion() != null){
+                            //It is possible to continue by using old data
+                            mainButton.setEnabled(true);
+                            mainButton.setText("START");
+                        }else{
+                            //If no data is downloaded previously, it is impossible to continue. Please retry under network environment
+                        }
+                    }
+                }, MainActivity.this);
+            }
+
+            @Override
+            public void onLatestVersion() {
+                Log.d("MainActivity", "onLatestVersion");
+                Toast.makeText(MainActivity.this, "On latest version", Toast.LENGTH_SHORT);
+                mainButton.setEnabled(true);
+                mainButton.setText("START");
+            }
+        }, this);
+    }
+}
