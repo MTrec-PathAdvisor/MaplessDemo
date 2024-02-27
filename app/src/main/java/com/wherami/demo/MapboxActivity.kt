8b73ac7f86f22bafa1a94188eeca75c99dc73d5c
peterTsui <wltsuiac@connect.ust.hk>
2024-02-27 14:55:37 +0800

    add mapbox view to visualize IA location

---
+++ app/src/main/java/com/wherami/demo/MapboxActivity.kt
@@ -0,0 +1,155 @@
+package com.wherami.demo
+
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.ImageHolder
+import com.mapbox.maps.MapView
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.gestures.OnMoveListener
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.LocationProvider
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import wherami.lbs.sdk.adaptive.IALocation
+public class MapboxActivity :AppCompatActivity() {
+    private var handler: Handler? = null
+    private var mLocation: IALocation? = null
+    private val latestHeading = 0f
+    var TAG = "MapboxActivity"
+    private lateinit var mapView: MapView
+    lateinit var permissionsManager: PermissionsManager
+    private lateinit var iaLocationProvider: LocationProvider
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Permission sensitive logic called here, such as activating the Maps SDK's LocationComponent to show the device's location
+        } else {
+            permissionsManager = PermissionsManager(permissionsListener)
+            permissionsManager.requestLocationPermissions(this)
+        }
+
+        mapView = MapView(this)
+        mapView.getMapboxMap().apply {
+            setCamera(
+                CameraOptions.Builder()
+                    .center(Point.fromLngLat(114.26384, 22.33615)) // camera at HKUST
+                    .zoom(15.5)
+                    .build()
+            )
+        }
+        mapView.gestures.addOnMoveListener(onMoveListener)
+        // Display user location
+        mapView.location.updateSettings {
+            puckBearing = PuckBearing.COURSE
+            puckBearingEnabled = true
+            enabled = true
+            locationPuck = LocationPuck2D(
+                bearingImage = ImageHolder.from(R.drawable.mapbox_user_puck_icon),
+                shadowImage = ImageHolder.from(R.drawable.mapbox_user_icon_shadow),
+                scaleExpression = interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0.0)
+                        literal(0.2)
+                    }
+                    stop {
+                        literal(20.0)
+                        literal(0.5)
+                    }
+                }.toJson()
+            )
+        }
+        mapView.location.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+
+        handler = Handler()
+
+        iaLocationProvider = IALocationProvider(this);
+        mapView.location.setLocationProvider(iaLocationProvider);
+        mapView.location.addOnIndicatorPositionChangedListener(
+            OnIndicatorPositionChangedListener { point: Point ->
+                Log.d("Mapbox locChange lon:",point.longitude().toString() +",lat:"+point.latitude().toString())
+            }
+        )
+
+
+        setContentView(mapView)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.location.removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.location.removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+    }
+
+    var runnableUi = Runnable { //更新界面
+        Log.i(TAG, "run: " + mLocation.toString())
+    }
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
+        mapView.getMapboxMap().setCamera(CameraOptions.Builder().bearing(it).build())
+    }
+
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
+//        Log.d("OnPosChangedListener", it.toString())
+        mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
+        mapView.gestures.focalPoint = mapView.getMapboxMap().pixelForCoordinate(it)
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {}
+    }
+
+    private fun onCameraTrackingDismissed() {
+        Toast.makeText(this, "onCameraTrackingDismissed", Toast.LENGTH_SHORT).show()
+        mapView.location
+            .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location
+            .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+    var permissionsListener: PermissionsListener = object : PermissionsListener {
+        override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+
+        }
+
+        override fun onPermissionResult(granted: Boolean) {
+            if (granted) {
+
+                // Permission sensitive logic called here, such as activating the Maps SDK's LocationComponent to show the device's location
+
+            } else {
+
+                // User denied the permission
+
+            }
+        }
+    }
+}
